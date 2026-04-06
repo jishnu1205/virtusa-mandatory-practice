bfec027642fe056dcbdd0143dd0565c8c355edb0
jishnu1205 <jishnuraghav1@gmail.com>
2026-04-06 13:29:35 +0530

    inserted sample data

---
+++ sql-digi-lib/random_data.sql
@@ -0,0 +1,14 @@
+insert into books values (1, 'Data Strutures', 'Science'), 
+(2, 'World History', 'History'),
+(3, 'Python', 'Science'),
+(4, 'Conspiracies', 'Fiction');
+
+insert into students values (101, 'Nikhil', '2005-10-09'),
+(102, 'Jai', '2006-07-18'),
+(103, 'Jishnu', '2005-02-01'),
+(104, 'Harsha', '2005-06-09');
+
+insert into issuedBooks values (1, 1, 101, '2026-03-01', NULL),
+(2, 2, 102, '2026-03-20', '2026-03-25'),
+(3, 3, 103, '2026-02-10', NULL),
+(4, 4, 104, '2026-03-28', NULL);
